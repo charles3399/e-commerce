4b95aa1f3c50d95afb24cbcea2f785ada6f47a9d
charles3399 <bernaldezsay@gmail.com>
2021-12-13 11:55:35 +0800

    installed some dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Welcome to Ecommerce!</title>
-</head>
-<body>
-    <div class="bg-slate-600 container text-center text-white">
-        <h1>Welcome to E-commerce!</h1>
-    </div>
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Welcome to Ecommerce!</title>
+    </head>
+    <body>
+        <div class="bg-slate-800 container text-center text-white p-5">
+            <h1 class="text-xl font-bold">Welcome to E-commerce!</h1>
+            <p class="text-sm italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, qui. Ea debitis quo necessitatibus odio placeat neque laudantium! Voluptatem nihil ullam quisquam alias vitae laborum! Quod aliquid harum tempora corporis?</p>
+        </div>
+    </body>
 </html>
